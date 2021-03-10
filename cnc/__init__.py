c0164a9b8821fe1d2f945d7e35648af576a24496
Jaehwan <chrisjae508@naver.com>
2021-03-11 00:12:09 +0900

    [Feat] blueprint 적용

--- cnc/__init__.py
+++ cnc/__init__.py
@@ -1,12 +1,28 @@
 from flask import Flask
 
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__)  # 모듈명
 
-    @app.route('/')
-    def hello_cnc():
-        return 'Hello, C&C!'
+    app.config.from_object(config)  # app.config 환경 변수
+
+    # ORM
+    db.init_app(app)  # 초기화
+    migrate.init_app(app, db)  # 초기화
+
+    from . import models  # 데이터베이스 모델
+
+    # 블루프린트
+    from .views import main_views
+
+    app.register_blueprint(main_views.bp)
 
     return app
-
