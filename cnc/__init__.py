9dec78b8d92b10d63703dc0754645b2e7b62fe54
Jaehwan <chrisjae508@naver.com>
2021-03-16 02:25:29 +0900

    [Feat] 404 에러핸들러 설정

--- cnc/__init__.py
+++ cnc/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -9,10 +9,17 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 def create_app():
     app = Flask(__name__)  # 모듈명
 
     app.config.from_object(config)  # app.config 환경 변수
+
+    # FLASK_JSON configuration
+    app.config['JSON_ADD_STATUS'] = False
 
     # ORM
     db.init_app(app)  # 초기화
@@ -24,5 +31,8 @@ def create_app():
     from .views import main_views
 
     app.register_blueprint(main_views.bp)
+
+    # 오류 페이지
+    app.register_error_handler(404, page_not_found)
 
     return app
